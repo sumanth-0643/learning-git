e58a14934ae3b400de953a4f1d2559e9790d018d
Sumanth S S <sumanthss025@gmail.com>
2025-01-12 08:41:31 +0530

    Revert "four commits together"
    This reverts commit d1790367afcbc21e3fa031f6c347197714681bae.

--- script.js
+++ script.js
@@ -8,10 +8,3 @@ function sayHi(){
     console.log('Hello World!!')
 }
 sayHi()
-
-const carDetails = {
-    carBrand: 'Volkswagen',
-    carModel: 'Virtus',
-    carRegNo: 'KA14PS0205'
-}
-console.log(carDetails)
